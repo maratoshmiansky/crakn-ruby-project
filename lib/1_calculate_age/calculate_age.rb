a5c5b6c91459a7cf7da8885e403306ff32ebc6c9
Marat Oshmiansky <maratical@gmail.com>
2021-12-05 23:47:40 -0800

    initial commit - add 2 of 3 problems

--- lib/1_calculate_age/calculate_age.rb
+++ lib/1_calculate_age/calculate_age.rb
@@ -1,5 +1,12 @@
+# frozen_string_literal: true
+
 class CalculateAge
   def self.calculate(age_at_birth, age_at_death)
-    # Write your code here
+    return "death date must be greater than birth date" if age_at_birth > age_at_death
+
+    # 1) subtract birth year from death year to get initial value
+    # 2) subtract 1 from initial value IF death month < birth month OR (death month = birth month AND death day < birth day) to get correct age (yrs)
+    # 3) return correct age
+    age_at_death.year - age_at_birth.year - ((age_at_death.mon < age_at_birth.mon) || (age_at_death.mon == age_at_birth.mon && age_at_death.mday < age_at_birth.mday) ? 1 : 0)
   end
 end
--- lib/2_match_person/person_matcher.rb
+++ lib/2_match_person/person_matcher.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class PersonMatcher
   attr_reader :people
 
@@ -5,5 +7,18 @@ class PersonMatcher
     @people = people
   end
 
-  # write your find_match method here
+  # set each keyword parameter value in find_match to "" by default
+  def find_match(first_name: "", middle_name: "", last_name: "")
+    # return empty array if no name data passed
+    return [] if first_name.empty? && middle_name.empty? && last_name.empty?
+
+    # select all people with matching name arguments only (filtering out missing name argument(s))
+    @people.select do |person|
+      first_name_condition = person[:first_name] == first_name || first_name.empty?
+      middle_name_condition = person[:middle_name] == middle_name || middle_name.empty?
+      last_name_condition = person[:last_name] == last_name || last_name.empty?
+
+      first_name_condition && middle_name_condition && last_name_condition
+    end
+  end
 end
