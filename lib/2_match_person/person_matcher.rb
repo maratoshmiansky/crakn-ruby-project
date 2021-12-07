6e4eb525859f190ecaf3ff8ae0c3c15f996b1e16
Marat Oshmiansky <maratical@gmail.com>
2021-12-06 22:47:35 -0800

    delegate find_match select method subconditions to methods

--- lib/2_match_person/person_matcher.rb
+++ lib/2_match_person/person_matcher.rb
@@ -7,18 +7,28 @@ class PersonMatcher
     @people = people
   end
 
-  # set each keyword parameter value in find_match to "" by default
   def find_match(first_name: "", middle_name: "", last_name: "")
+    @first_name = first_name
+    @middle_name = middle_name
+    @last_name = last_name
     # return empty array if no name data passed
     return [] if first_name.empty? && middle_name.empty? && last_name.empty?
 
-    # select all people with matching name arguments only (filtering out missing name argument(s))
+    # only select people with matching first, middle and/or last name(s) and return the array
     @people.select do |person|
-      first_name_condition = person[:first_name] == first_name || first_name.empty?
-      middle_name_condition = person[:middle_name] == middle_name || middle_name.empty?
-      last_name_condition = person[:last_name] == last_name || last_name.empty?
-
-      first_name_condition && middle_name_condition && last_name_condition
+      first_name_condition(person) && middle_name_condition(person) && last_name_condition(person)
     end
+  end
+
+  def first_name_condition(person)
+    person[:first_name] == @first_name || @first_name.empty? # was a first name passed?
+  end
+
+  def middle_name_condition(person)
+    person[:middle_name] == @middle_name || @middle_name.empty? # was a middle name passed?
+  end
+
+  def last_name_condition(person)
+    person[:last_name] == @last_name || @last_name.empty? # was a last name passed?
   end
 end
